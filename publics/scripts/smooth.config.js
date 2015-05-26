0ec0019e81ae0514d9723ba91d498ea3889a3b7f
shaomingquan <1218525419@qq.com>
2015-05-26 22:09:55 +0800

    fix css

--- publics/scripts/smooth.config.js
+++ publics/scripts/smooth.config.js
@@ -6,7 +6,7 @@ links.each(function(){
 	$(this).click(function(e){
 		var id = $(this).attr("href").substring(1);
 		name="1428232557373"
-		$.smoothScroll({scrollTarget: "[name="+id+"]"});
+		$.smoothScroll({scrollTarget: "[id="+id+"]"});
 		e.preventDefault();
 		return false;
 	})
