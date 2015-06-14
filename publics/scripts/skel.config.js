b8307a98a83bc47d127f8cdae6f73ca42eb1fc90
shaomingquan <1218525419@qq.com>
2015-06-14 14:43:41 +0800

    already

--- publics/scripts/skel.config.js
+++ publics/scripts/skel.config.js
@@ -4,17 +4,17 @@ skel.init({
   breakpoints: {
     large : {
       media: '(min-width: 1080px)',
-      href : '/blog/publics/stylesheets/large.css'
+      href : '/publics/stylesheets/large.css'
     },
     medium: {
       media: '(min-width: 480px) and (max-width: 1080px)',
       containers : "720px",
-      href : '/blog/publics/stylesheets/large.css'
+      href : '/publics/stylesheets/large.css'
     },
     small: {
       media: '(max-width: 480px)',
       containers : "100%",
-      href : '/blog/publics/stylesheets/small.css'
+      href : '/publics/stylesheets/small.css'
     }
   }
 });
