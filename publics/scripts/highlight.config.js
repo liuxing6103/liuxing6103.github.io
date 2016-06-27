6971283cbcbfef0852f4c7fb2dafc5e30d510f25
邵明全 <shaomingquan@shaomingquandeMacBook-Air.local>
2016-06-27 23:01:10 +0800

    change all

--- publics/scripts/highlight.config.js
+++
@@ -1,8 +0,0 @@
-// highlight.config.js
-(function(){
-
-	$("pre ,code").each(function(i ,block){
-		hljs.highlightBlock(block);
-	})
-
-})()
--- publics/scripts/menu.config.js
+++
@@ -1,6 +0,0 @@
-(function(){
-	
-var mM = new markdownMenu("blogStage","blogMenuStage");
-mM.initDom({title:"<i>Contents</i>"});
-
-})()
